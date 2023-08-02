abf380d74a906ce4fd13d48a34760a898fb73ac8
Hassan Jarko <hassan.jarko@yahoo.com>
2023-08-02 23:00:23 +0400

    added Home screen temp UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,22 @@ export default function Home() {
   console.log(session);
   return (
     <>
-      <h1 className="text-center text-4xl">{session?.user?.email}</h1>
+      <div className="h-screen w-screen flex items-center justify-center">
+        {session ? (
+          <div className="flex flex-col gap-1 items-center">
+            <h2>{session?.user?.name}</h2>
+            <img
+              src={session.user?.image!}
+              alt="user image"
+              className="w-32 h-32 rounded-full"
+            />
+            <h4>{session.user?.email}</h4>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <button onClick={() => signIn()}>Sign in</button>
+        )}
+      </div>
     </>
   );
 }
